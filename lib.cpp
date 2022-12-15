f1901b8359d31ff63b5e5bef7f3291c23ab659c4
mattemonte03 <117922070+mattemonte03@users.noreply.github.com>
2022-12-16 00:27:45 +0100

    Update lib.cpp

--- lib.cpp
+++ lib.cpp
@@ -1,15 +1,12 @@
 #include "lib.h"
 #include <cstring>
-#include <iostream>
-using namespace std;
 float Array_Search(char lista[10][20], char nome[1][20])
 {
     float posizione;
     posizione = -1;
 
     for(int i=0;i<3;i++)
-    {
-  
+    {  
         if (strcmp(lista[i], nome[0]) == 0) {
            posizione=i;
         }
