d870e0fad8e0b545942dfbcb871cf20986f7c55e
mattemonte03 <117922070+mattemonte03@users.noreply.github.com>
2022-12-16 00:54:34 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,22 +5,22 @@ using namespace std;
 
 int main() {
 
-    char lista[10][20];
-    char nome[1][20];
+   char lista[10][20];
+   char nome[1][20];
 
-   cout<<"inserisci 10 nomi: "<<endl;
+   cout << "inserisci 10 nomi: " << endl;
    for(int i=0;i<10;i++){
-        cin>>lista[i];
+        cin >> lista[i];
      }
-    cout<<"Inserisci nome da cercare: "<<endl;
-    cin>>nome[0];
+    cout << "Inserisci nome da cercare: " << endl;
+    cin >> nome[0];
 
     float pos = Array_Search (lista,nome);
 
     if (pos<0) {
-        cout <<"Non presente";
+        cout << "Non presente";
     } else {
-        cout <<pos;
+        cout << pos;
     }
 
 return 0;
